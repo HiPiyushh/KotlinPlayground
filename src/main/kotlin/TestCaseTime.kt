a60a96d3b0e8e95aebc8fbbda7198b5823f3fb72
HiPiyushh <hipiyushh@gmail.com>
2023-01-02 11:55:31 +0530

    Available

--- src/main/kotlin/TestCaseTime.kt
+++ src/main/kotlin/TestCaseTime.kt
@@ -3,11 +3,10 @@ fun main() {
     val input = Scanner(System.`in`)
     val t = input.nextInt()
     repeat(t) {
-        val x = input.nextInt()
-        val y = input.nextInt()
-        val z = input.nextInt()
+        val a = input.nextInt()
+        val b = input.nextInt()
+        val c = input.nextInt()
 
-        if (y <= 10*x) println(y*z)
-        else println(10*x*z)
+         println((c-b)/a)
 }
 }
