f7a6e2b6d89b8358b51f0ba18940cdf1ca4acaa8
HiPiyushh <hipiyushh@gmail.com>
2023-01-03 13:23:19 +0530

    TotalCostIncurred

--- src/main/kotlin/TestCaseTime.kt
+++ src/main/kotlin/TestCaseTime.kt
@@ -3,10 +3,10 @@ fun main() {
     val input = Scanner(System.`in`)
     val t = input.nextInt()
     repeat(t) {
-        val c = input.nextInt()
+        val n = input.nextInt()
         val x = input.nextInt()
-        val y = input.nextInt()
 
-         println((c-x)*y)
+
+        println(if(n <= 6) x else if(n%6 == 0) (n/6)*x else ((n/6)+1)*x)
 }
 }
