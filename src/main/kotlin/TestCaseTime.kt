e193faf30ee72bba584ef57b256cea417f857adf
HiPiyushh <hipiyushh@gmail.com>
2023-06-09 13:41:45 +0530

    availabilityOfSeats

--- src/main/kotlin/TestCaseTime.kt
+++ src/main/kotlin/TestCaseTime.kt
@@ -1,11 +1,15 @@
 import java.util.Scanner
 fun main() {
-    val input = Scanner(System.`in`)
+    val input  = Scanner(System.`in`)
     val t = input.nextInt()
 
     repeat(t) {
-        val a = input.nextInt()
-        val b = input.nextInt()
-        if(a*15>=b * 2) println("Yes") else println("No")
+        val n = input.nextInt()
+        val m = input.nextInt()
+        val k = input.nextInt()
+
+        if (m-k >= n) {println("Yes") } else {
+            println("No") }
+        }
+
     }
-}
