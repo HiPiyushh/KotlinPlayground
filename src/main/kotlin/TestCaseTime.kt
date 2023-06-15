96cdd2e96dbbdc0f237b639e2b15ccb6a72dfe6e
HiPiyushh <hipiyushh@gmail.com>
2023-06-15 12:11:37 +0530

    musicalInstrument

--- src/main/kotlin/TestCaseTime.kt
+++ src/main/kotlin/TestCaseTime.kt
@@ -2,9 +2,7 @@ import java.util.Scanner
 fun main() {
     val input = Scanner(System.`in`)
     val x = input.nextInt()
-    val y = input.nextInt()
 
-
-    val numberOfPacket = (x * y)
-    println(numberOfPacket)
+    if ( x + 7 >= 170)  println("Yes")
+    else  println("No")
 }
