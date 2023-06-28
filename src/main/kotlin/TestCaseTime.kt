49528d9c27b6a02adbcf498ceaac780dba9ae02e
HiPiyushh <hipiyushh@gmail.com>
2023-06-28 12:02:02 +0530

    Cred Amount

--- src/main/kotlin/TestCaseTime.kt
+++ src/main/kotlin/TestCaseTime.kt
@@ -3,10 +3,9 @@ fun main() {
     val input = Scanner(System.`in`)
     val t = input.nextInt()
     repeat(t) {
-        val x = input.nextInt()
-        val y = input.nextInt()
-
-
-        println((x*10)+(y*5))
+        val a = input.nextInt()
+        val b = input.nextInt()
+        val c = a*b
+        println(c/100)
 }
 }
