0576279460a5f7bc4edcbe7c191e066fa50fdbab
HiPiyushh <hipiyushh@gmail.com>
2023-07-03 11:22:21 +0530

    ReceiveYesOrNo

--- src/main/kotlin/Testing1.kt
+++ src/main/kotlin/Testing1.kt
@@ -1,2 +1,20 @@
-// Paytm Payment Gateway
-// Payment Gateway - PayUMoney
+import java.util.Scanner
+
+fun main() {
+    val input = Scanner(System.`in`)
+    val t = input.nextInt()
+    repeat(t) {
+    val a = input.nextInt()
+    val b = input.nextInt()
+    val x = input.nextInt()
+    val y = input.nextInt()
+
+        if (x*y>=a*b) {
+            println("Yes")
+        }
+        else{
+            println("No")
+        }
+
+    }
+}
